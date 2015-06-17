12708465c1c1620a0af1197bcf737ea3e7774e18
John Fu <fuqcool@gmail.com>
2015-06-17 09:40:52 -0400

    I actually put passwords on github

---
+++ blackbox.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+
+if [ $# -eq 0 ]; then
+    echo "No arguments supplied."
+    exit 1
+fi
+
+FILE=$1
+ENCRYPT_METHOD=-aes-256-cbc
+
+export PASSWORD
+
+if [ ! -f $FILE ]; then
+    read -s -p "Please initialize your password: " PASSWORD
+    echo
+    echo "Anything you put in here will be encrypted. Shhhhh...." > $FILE.tmp
+else
+    read -s -p "Please enter your password: " PASSWORD
+    echo
+    openssl enc $ENCRYPT_METHOD -d -in $FILE -pass "env:PASSWORD" > $FILE.tmp
+
+    RESULT=$?
+    if [ ! $RESULT -eq 0 ]; then
+        echo "Cannot open decrypted file."
+        rm $FILE.tmp
+        exit 1
+    fi
+fi
+
+DATE_BEFORE=$(stat -f "%Sm" $FILE.tmp)
+vim $FILE.tmp
+DATE_AFTER=$(stat -f "%Sm" $FILE.tmp)
+
+if [ "$DATE_BEFORE" = "$DATE_AFTER" ]; then
+    rm $FILE.tmp
+    echo "No changes made, exit."
+    exit 0
+fi
+
+RESULT=$?
+if [ $RESULT -eq 0 ]; then
+    echo "Encrypt $FILE ..."
+    openssl enc $ENCRYPT_METHOD -in $FILE.tmp -out $FILE -pass "env:PASSWORD"
+fi
+if [ -f $FILE.tmp ]; then
+    rm $FILE.tmp
+fi
+
+unset PASSWORD
+echo "Done."
